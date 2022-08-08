83761bff23e9929cd1c57c2b73b6e11e76e0845a
LokeshRedd555 <reddylokesh448@gmail.com>
2022-08-08 16:42:14 +0530

    Pattern pg

---
+++ Excercises/src/Pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char ch1 = sc.next().charAt(0);
+        char ch2 = sc.next().charAt(0);
+        for(int i=0;i<n;i++) {              //rows
+            for(int j=0;j<n;j++) {          //columns
+                if(j<=i) {
+                    System.out.print(ch1+" ");
+                }
+                else {
+                    System.out.print(ch2+" ");
+                }
+            } System.out.println();
+        }
+    }
+}
