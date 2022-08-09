7002c84817de9e48bdd294e717b54b775e258a69
LokeshRedd555 <reddylokesh448@gmail.com>
2022-08-09 13:05:44 +0530

    added Mode functionality for an array

--- Excercises/src/daytwo/MeanModeMedian.java
+++ Excercises/src/daytwo/MeanModeMedian.java
@@ -11,6 +11,7 @@ public class MeanModeMedian {
             array[i] = sc.nextInt();
         }
         System.out.println(mean(array));
+        System.out.println(mode(array));
     }
     public static float mean(int[] array) {
         float mean = 0, sum = 0;
@@ -19,5 +20,17 @@ public class MeanModeMedian {
         }
         mean = sum/array.length;
         return mean;
+    }
+    public static int mode(int[] array) {
+        int mode = 0, mostRepeating = 0;
+        int numbers[] = new int[10]; //considering 0 to 9 i/p numbers in array
+        for(int i=0;i<array.length;i++) {
+            numbers[array[i]]++;
+            if(numbers[array[i]] > mostRepeating) {
+                mostRepeating = numbers[array[i]];
+                mode = array[i];
+            }
+        }
+        return mode;
     }
 }
